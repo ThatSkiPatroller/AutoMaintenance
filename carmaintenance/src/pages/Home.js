3f2de65a7c10c5b674403b97d6f1e5f2d86299fe
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-05-30 23:36:33 -0500

    Added signup and working, added navbar component and homepage for signup

---
+++ carmaintenance/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signup from '../components/Signup/Signup';
+import Navbar from '../components/Navbar/Navbar'
+
+function Home () {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Signup></Signup>
+        </div>
+    )
+}
+
+export default Home;
