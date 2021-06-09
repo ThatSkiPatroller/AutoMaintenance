9f9c6686fe47e02b5b65721085b2dbccfb5fcc6e
Arti <arti.iosdev@gmail.com>
2021-06-09 10:41:38 -0700

    cleanuo

--- models/index.js
+++ models/index.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   User: require('./User'),
-  Clinic: require('./clinic'),
-  Dealer: require('./Dealer'),
-  HealthLog: require('./healthlog'),
   Appointment: require('./appointment'),
-  SymptomJournal: require('./symptom'),
-  Prescription: require('./prescription'),
-  //Code: require('./Code'),
-  //Token: require('./token'),
-  //Client: require('./Client'),
+  
 };
